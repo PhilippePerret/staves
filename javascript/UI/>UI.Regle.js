19c916d9cb0d001ca6f738fdcf4cb8fb2124a447
Philippe Perret <philippe.perret@yahoo.fr>
2014-01-14 08:12:22 +0100

    Amélioration règle (#52)

--- javascript/UI/>UI.Regle.js
+++ javascript/UI/>UI.Regle.js
@@ -33,13 +33,40 @@ window.UI.Regle = {
     this.obj.hide()
   },
   /**
-    * Prépare la règle
+    * Prépare/construit la règle
     * @method prepare
     */
   prepare:function(){
     this.obj = $('div#regle')
+    this.construit_graduations()
     this.obj.
       draggable({containment:'document'}).
       css({top:'480px', left:'25px'})
+  },
+  /**
+    * Construction de la graduation de la règle
+    * @method construit_graduations
+    */
+  construit_graduations:function()
+  {
+    var x = 0
+    while(x < 200)
+    {
+      this.obj.append(this.boite_graduation(x))
+      x += 12
+    }
+  },
+  /**
+    * Retourne le code HTML pour la graduation +x+
+    * Note : cela consiste en deux boites imbriquées, l'une pour le trait de
+    * graduation et l'autre pour la valeur de la graduation
+    * @method boite_graduation
+    * @param  {Number} x  Valeur de la graduation
+    */
+  boite_graduation:function(x)
+  {
+    return  '<div class="divregle" style="left:'+x+'px;">'+
+              '<div class="mesregle">'+(x?x:"")+'</div>'+
+            '</div>'
   }
 }
